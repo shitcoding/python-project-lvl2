be936d3cfac603ab5e64575eb45d87d39b22519f
Shitcoder <shitcoder@pm.me>
2020-07-22 23:22:57 +0300

    fixed issues with gendiff module import

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -27,27 +27,29 @@ def generate_diff(path_to_file1, path_to_file2):
     return output.lower()
 
 
-# Parsing arguments from CLI.
-parser = argparse.ArgumentParser(description='Generate diff')
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
-parser.add_argument('-f', '--format', type=str, help='set format of output')
-args = parser.parse_args()
-
-
-# Handling absolute and relative paths.
-if os.path.isabs(args.first_file):
-    path_to_file1 = args.first_file
-else:
-    path_to_file1 = os.path.abspath(args.first_file)
-if os.path.isabs(args.second_file):
-    path_to_file2 = args.second_file
-else:
-    path_to_file2 = os.path.abspath(args.second_file)
-
 
 def main():
-    """Generate diff string between first_file and second_file."""
+    """
+    Generate diff string between first_file and second_file.
+    """
+    # Parsing arguments from CLI.
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
+    args = parser.parse_args()
+
+    # Handling absolute and relative paths.
+    if os.path.isabs(args.first_file):
+        path_to_file1 = args.first_file
+    else:
+        path_to_file1 = os.path.abspath(args.first_file)
+    if os.path.isabs(args.second_file):
+        path_to_file2 = args.second_file
+    else:
+        path_to_file2 = os.path.abspath(args.second_file)
+
+
     diff = generate_diff(path_to_file1, path_to_file2)
     print(diff)
 
