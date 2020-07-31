09d36db68080bff71de01c9d8e86bf2878188492
Shitcoder <shitcoder@pm.me>
2020-07-31 17:26:33 +0300

    added tests for default diff view

---
+++ tests/test_default.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+"""Diff generation tests."""
+
+from gendiff.diff import generate_diff
+from gendiff.views import default_view
+
+
+def test_default():
+    with open('./tests/fixtures/1__expected_default.txt', 'r') as fixture:
+        # rstrip() to get rid of \n in the end
+        expected = fixture.read().rstrip()
+
+        assert expected == generate_diff(
+                './tests/fixtures/1__before.json',
+                './tests/fixtures/1__after.json'
+        )
